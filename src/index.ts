583404d4f8c6d35611c25710df6ca605e2d63d3f
king <117745900@qq.com>
2020-11-13 10:40:49 +0800

    fix: 子模块动态加载

--- src/index.ts
+++ src/index.ts
@@ -86,7 +86,7 @@ export default function(api: IApi) {
                 content: `
                 if(window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__){
                   window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ += "${trimPath}";
-                  console.log(window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__);
+                  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
                 }
                 `,
               },
