2baecee3b1659efbd8dd469853f946f4b898c4d4
king <117745900@qq.com>
2021-06-03 17:42:05 +0800

    feat: 新增antd provide配置

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,9 @@ export function getContet(dictionary: string) {
   return `
 import React from 'react';
 import { useCoreState, useRequestDictionary, App } from '@sensoro/core';
-
-
+import { Empty } from '@sensoro/sensoro-design';
+import { ConfigProvider } from 'antd';
+import locale from 'antd/lib/locale/zh_CN';
 const Dictionary: React.FC = ({ keys, children }) => {
   const running = useRequestDictionary(keys);
   return (
@@ -27,11 +28,13 @@ export default (props) => {
   return (
     <>
       {running && (
-        <Dictionary keys={${dictionary}}>
-          <App {...rest}>
-            {children}
-          </App>
-        </Dictionary>
+        <ConfigProvider locale={locale} autoInsertSpaceInButton={false} renderEmpty={() => <Empty />}>
+          <Dictionary keys={${dictionary}}>
+            <App {...rest}>
+              {children}
+            </App>
+          </Dictionary>
+        </ConfigProvider>
       )}
     </>
   );
